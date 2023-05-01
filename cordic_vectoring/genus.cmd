c8501af8861e84cdf58e29dd1be47056fe036154
PAPPU KUMAR <is22mtech14003@iith.ac.in>
2023-05-01 12:43:03 +0530

    Add files via upload

---
+++ cordic_vectoring/genus.cmd
@@ -0,0 +1,13 @@
+# Cadence Genus(TM) Synthesis Solution, Version 20.10-p001_1, built Dec 11 2020 16:29:55
+
+# Date: Fri Apr 28 02:00:34 2023
+# Host: DIGITAL-SERVER (x86_64 w/Linux 3.10.0-1160.36.2.el7.x86_64) (8cores*64cpus*4physical cpus*Intel(R) Xeon(R) CPU E5-4650 0 @ 2.70GHz 20480KB)
+# OS:   Red Hat Enterprise Linux Server release 7.9 (Maipo)
+
+source rotating_vectoring.tcl
+source rotating_vectoring.tcl
+source rotating_vectoring.tc
+source rotating_vectoring.tcl
+source rotating_vectoring.tcl
+cd pappu
+cd ..//
---
+++ vectoring/genus.cmd
@@ -0,0 +1,14 @@
+# Cadence Genus(TM) Synthesis Solution, Version 20.10-p001_1, built Dec 11 2020 16:29:55
+
+# Date: Fri Apr 28 00:25:06 2023
+# Host: DIGITAL-SERVER (x86_64 w/Linux 3.10.0-1160.36.2.el7.x86_64) (8cores*64cpus*4physical cpus*Intel(R) Xeon(R) CPU E5-4650 0 @ 2.70GHz 20480KB)
+# OS:   Red Hat Enterprise Linux Server release 7.9 (Maipo)
+
+sosource vectoring.tcl
+source vectoring.tcl
+source vectoring.tcl
+cd pappu
+pappu
+mkdir rotating_vectoring
+cd pappu
+exit
